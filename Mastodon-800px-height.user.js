d88c5053f90cb4daa9ac6ae91cd65018f7746973
Letiteuf55 <arthur.lemoine@me.com>
2018-01-31 11:07:49 +0100

    Add Mastodon 800px height

---
+++ Mastodon-800px-height.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Mastodon 800px height
+// @namespace    http://crazypanda.Fr
+// @version      0.1
+// @description  Change default Mastodon Theme and adding 800px height for pictures.
+// @author       Arthur Lemoine
+// @match        *://*/web/*
+
+// @grant        GM_addStyle
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+GM_addStyle ('.media-gallery { height: auto !important; max-height: 800px !important;}');
+})();
+
